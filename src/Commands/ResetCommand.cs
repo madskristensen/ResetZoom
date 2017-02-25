fc99918867ba3d4f74f2531c0a8adeae40f265ed
Mads Kristensen <post@madskristensen.net>
2017-02-24 16:02:11 -0800

    Add project files.

---
+++ src/Commands/ResetCommand.cs
@@ -0,0 +1,90 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System;
+using System.ComponentModel.Design;
+
+namespace ResetZoom
+{
+    internal sealed class ResetCommand
+    {
+        private readonly Package _package;
+
+        private ResetCommand(Package package)
+        {
+            _package = package;
+
+            if (ServiceProvider.GetService(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
+            {
+                var menuCommandID = new CommandID(PackageGuids.guidPackageCmdSet, PackageIds.ResetCommand);
+                var menuItem = new MenuCommand(Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        public static ResetCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get { return _package; }
+        }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new ResetCommand(package);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            var dte = (DTE2)ServiceProvider.GetService(typeof(DTE));
+
+            if (dte.ActiveDocument == null)
+                return;
+
+            IWpfTextView view = GetCurentTextView();
+
+            if (view == null || view.ZoomLevel == 100)
+                return;
+
+            if (view.ZoomLevel > 100)
+            {
+                while (view.ZoomLevel > 100)
+                    dte.ExecuteCommand("View.ZoomOut");
+            }
+            else
+            {
+                while (view.ZoomLevel < 100)
+                    dte.ExecuteCommand("View.ZoomIn");
+            }
+        }
+
+        public IWpfTextView GetCurentTextView()
+        {
+            return GetTextView();
+        }
+
+        public IWpfTextView GetTextView()
+        {
+            var compService = ServiceProvider.GetService(typeof(SComponentModel)) as IComponentModel;
+            IVsEditorAdaptersFactoryService editorAdapter = compService.GetService<IVsEditorAdaptersFactoryService>();
+            return editorAdapter.GetWpfTextView(GetCurrentNativeTextView());
+        }
+
+        public IVsTextView GetCurrentNativeTextView()
+        {
+            var textManager = (IVsTextManager)ServiceProvider.GetService(typeof(SVsTextManager));
+
+            ErrorHandler.ThrowOnFailure(textManager.GetActiveView(1, null, out IVsTextView activeView));
+            return activeView;
+        }
+    }
+}
---
+++ src/ResetZoomPackage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace ResetZoom
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidPackageString)]
+    public sealed class ResetZoomPackage : Package
+    {
+        protected override void Initialize()
+        {
+            ResetCommand.Initialize(this);
+        }
+    }
+}
