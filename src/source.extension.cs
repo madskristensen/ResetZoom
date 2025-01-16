e1cccef57f21751450953a67f37c18384eaa0499
Mads Kristensen <madsk@microsoft.com>
2025-01-16 13:05:37 -0800

    Release v1.3

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,10 +9,10 @@ namespace ResetZoom
     {
         public const string Id = "0a987da3-4577-4eec-aaa9-a4c2cdd5d22b";
         public const string Name = "Reset Zoom";
-        public const string Description = @"Adds a command to reset the editor zoom level to 100% with a customizable keyboard shortcut";
+        public const string Description = @"Adds a command to reset the editor zoom level to 100% with a customizable keyboard shortcut. Defaults is Ctrl+0";
         public const string Language = "en-US";
-        public const string Version = "1.2.999";
+        public const string Version = "1.3";
         public const string Author = "Mads Kristensen";
-        public const string Tags = "zoom, reset";
+        public const string Tags = "zoom, reset, font, size";
     }
 }
